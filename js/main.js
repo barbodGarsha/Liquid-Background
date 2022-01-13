1708b4731e332a69378cbe8cd66d31e047a22c26
barbodGarsha <barbodgarshasbi79@gmail.com>
2022-01-13 13:40:54 +0100

    canvas ready for testing

--- js/main.js
+++ js/main.js
@@ -5,6 +5,14 @@ const canvas_max_x = canvas_w - 1
 const canvas_max_y = canvas_h - 1
 const ctx = canvas.getContext("2d")
 
+
+//Canvas Eventlisteners------------------------------------------
+canvas.addEventListener("mousemove", function(e){
+    
+})
+
+
+
 function draw_border(border_width) {
     ctx.fillRect(0, 0, canvas_w, canvas_max_y)
     ctx.clearRect(border_width - 1, border_width  - 1, canvas_max_x - (2 * border_width), canvas_max_y - (2 * border_width))
