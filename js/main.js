22cbd32f3b36a06289eac662b74a15c9db3975b7
barbodGarsha <barbodgarshasbi79@gmail.com>
2022-01-13 13:33:33 +0100

    we can now draw borders

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 const canvas = document.getElementById("test-canvas")
+const canvas_w = canvas.getBoundingClientRect().width;
+const canvas_h = canvas.getBoundingClientRect().height;
+const canvas_max_x = canvas_w - 1
+const canvas_max_y = canvas_h - 1
 const ctx = canvas.getContext("2d")
 
+function draw_border(border_width) {
+    ctx.fillRect(0, 0, canvas_w, canvas_max_y)
+    ctx.clearRect(border_width - 1, border_width  - 1, canvas_max_x - (2 * border_width), canvas_max_y - (2 * border_width))
+}
+
+draw_border(30)
