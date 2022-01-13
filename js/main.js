d7421aa3a0eaf58ac249701abe58b5ae47f7248f
barbodGarsha <barbodgarshasbi79@gmail.com>
2022-01-13 13:52:00 +0100

    get_mouse_position now returns the data

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const canvas = document.getElementById("test-canvas")
-const canvas_w = canvas.getBoundingClientRect().width;
-const canvas_h = canvas.getBoundingClientRect().height;
+const canvas_w = canvas.getBoundingClientRect().width
+const canvas_h = canvas.getBoundingClientRect().height
 const canvas_max_x = canvas_w - 1
 const canvas_max_y = canvas_h - 1
 const ctx = canvas.getContext("2d")
@@ -8,16 +8,17 @@ const ctx = canvas.getContext("2d")
 
 //Canvas Eventlisteners------------------------------------------
 canvas.addEventListener("mousemove", function(e){
-    get_mouse_position(canvas, e)
+    const pos = get_mouse_position(canvas, e)
+    console.log("Coordinate x: " + pos.mouse_pos_x, 
+                "Coordinate y: " + pos.mouse_pos_y)
 })
 
 
 function get_mouse_position(Canvas, e) {
-    let rect = Canvas.getBoundingClientRect();
+    let rect = Canvas.getBoundingClientRect()
     let x = e.clientX - rect.left;
     let y = e.clientY - rect.top;
-    console.log("Coordinate x: " + x, 
-                "Coordinate y: " + y);
+    return {mouse_pos_x : x, mouse_pos_y : y}
 }
 
 function draw_border(Border_width) {
